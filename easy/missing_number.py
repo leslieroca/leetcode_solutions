3efb72d05c6849bdf86e30ae1547b3875a6416c7
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-02-22 19:09:19 -0500

    Missing Number

---
+++ easy/missing_number.py
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/missing-number
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        s = sum(nums)
+        
+        return (n * (n + 1)//2) - s
+    
