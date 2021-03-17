c8d2e438964edac8bb526599f87f29314123e3d6
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-03-16 23:31:16 -0400

    Find the Duplicate Number

---
+++ medium/find_the_duplicate_number.py
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/find-the-duplicate-number
+
+class Solution:
+    def findDuplicate(self, nums):
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
