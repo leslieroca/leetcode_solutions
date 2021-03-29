772efedb244bb4bad87ac84d836934d39a430bb9
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-03-29 19:56:18 -0400

    Meeting Rooms

---
+++ easy/meeting_rooms.py
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/meeting-rooms
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) == 1:
+            return True
+        intervals.sort()
+        
+        for i in range(len(intervals) - 1):
+            if  intervals[i][1] > intervals[i + 1][0]:
+                return False
+             
+        return True
+        
