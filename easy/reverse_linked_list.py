c229af9d6062c301b71049315f989ebb22b5afb0
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-03-29 20:24:30 -0400

    Improve Reverse Linked List Solution

--- easy/reverse_linked_list.py
+++ easy/reverse_linked_list.py
@@ -6,16 +6,13 @@ https://leetcode.com/problems/reverse-linked-list
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev = None
         current = head
         
         while current:
-            next_current = current.next
-            current.next = prev
-            prev = current
-            current = next_current
-        
-        head = prev
-        return head
+            current.next, prev, current = prev, current, current.next
+
+        return prev
