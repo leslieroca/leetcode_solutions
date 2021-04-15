32ef65f242f68cad4f98278866b4a82dc4be1ee8
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-04-14 23:20:31 -0400

    Binary Search Using Bisect

---
+++ easy/binary_search_using_bisect.py
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/binary-search
+
+
+import bisect
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        index = bisect.bisect_left(nums, target)
+        if index < len(nums) and nums[index] == target:
+            return index
+        return -1
