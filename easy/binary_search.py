eb6b76fc0af880f72e9046a66c3caaa328948bd0
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-04-14 23:18:22 -0400

    Binary Search

---
+++ easy/binary_search.py
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/binary-search
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        min = 0
+        max = len(nums) -1 
+        
+        while min <= max:
+            mid = min + ((max - min) // 2)
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                min = mid + 1
+            else:
+                max = mid - 1
+                
+        return -1
