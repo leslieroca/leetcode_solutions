b2e3c24b3ef4666ea879c39f36cfb6be454a6d36
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-04-17 23:22:55 -0400

    Rotate Array

---
+++ medium/rotate_array.py
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/rotate-array
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        a = [0] * n
+        for i in range(n):
+            a[(i + k) % n] = nums[i]
+            
+        nums[:] = a
