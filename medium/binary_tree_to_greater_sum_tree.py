646e77007cf08f9d42c8a20712fc119f1acbff06
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-04-18 23:20:17 -0400

    Binary Search Tree to Greater Sum Tree

---
+++ medium/binary_tree_to_greater_sum_tree.py
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+            
+class Solution:
+    def __init__(self):
+        self.total = 0
+        
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root is not None:
+            self.bstToGst(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.bstToGst(root.left)
+        
+        return root
