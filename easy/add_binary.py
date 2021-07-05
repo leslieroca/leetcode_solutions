31461f4617ffc08db9ce2a7dc237ae7c077963f3
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-07-04 23:10:36 -0400

    Add Binary

---
+++ easy/add_binary.py
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/add-binary
+
+
+
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            x, y = x ^ y, (x & y) << 1
+            
+        return bin(x)[2:]
+
+
+
+
