2686a59aa0b7b90d178676ab61f4e27e650b6a0f
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-08-12 17:58:16 -0400

    Fibonacci Number

---
+++ easy/fibonacci_number.py
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/fibonacci-number
+
+
+# Recursive Solution 
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        
+        return self.fib(n - 1) + self.fib(n - 2)
+        
